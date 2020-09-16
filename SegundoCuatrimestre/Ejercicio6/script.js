27ed6fa0191d8a781c615ae3ab59f6f366a76907
FedericoCardozo <fedecardozo.a@gmail.com>
2020-09-16 14:50:55 -0300

    Ejercicio 6

---
+++ SegundoCuatrimestre/Ejercicio6/script.js
@@ -0,0 +1,36 @@
+
+function btnGuardar(){
+
+    var inputEdad = document.getElementById("edad");
+    var inputNombre = document.getElementById("nombre");
+
+    //alert(inputNombre.className);
+    if(inputNombre.value == ""){
+
+        inputNombre.className = "error";
+
+    }else{
+
+        inputNombre.className = "sinError";
+    }
+    if(inputEdad.value == ""){
+
+        inputEdad.className = "error";
+
+    }else{
+
+        if(inputEdad.value < 18){
+
+            inputEdad.className = "menor";
+
+        }else{
+
+            inputEdad.className = "sinError";
+
+        }
+
+        
+    }
+
+}
+
